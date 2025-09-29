412e9c53e455a96b82b9214f71a7be9c13848bc9
FrancescoMarchi <marchi.frncsc@gmail.com>
2025-09-30 00:52:45 +0700

    sql

---
+++ sql/rebuild_views.sql
@@ -0,0 +1,6 @@
+\set ON_ERROR_STOP 1
+SET search_path = retention, public;
+\i sql03_features.sql
+\i sql05_deciles.sql
+\i sql99_views_for_bi.sql
+\i sql10_analysis_basics.sql
---
+++ sql/sqlquick_rebuild(diff).sql
@@ -0,0 +1,4 @@
+\i sql00_schema.sql
+\i sql03_features.sql
+\i sql05_deciles.sql
+\i sql99_views_for_bi.sql
